884e1cc6417b96574e620a4f389ec2ec8f85ca64
jonathanmusila <jonathanmusila6@gmail.com>
2019-02-07 15:20:36 +0300

    [Finishes #163781891] Added login functionality

--- UI/static/js/login.js
+++ UI/static/js/login.js
@@ -1,8 +1,8 @@
 // User Login
-login = () => {
+signin = () => {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    let loginUrl = 'http://127.0.0.1:5000/api/v2/auth/login';
+    let loginUrl = 'https://my-postgres-questioner-v2-api.herokuapp.com/api/v2/auth/login';
     fetch(loginUrl, {
             method: 'POST',
             headers: {
@@ -13,16 +13,16 @@ login = () => {
         })
         .then((res) => res.json())
         .then((data) => {
-            if (data.status === 'Failed!') {
+            if (data.Status === 400) {
                 // if request is unsuccessful
-                document.getElementById('output').style.color = 'red'
-                document.getElementById('output').innerHTML = data.message
+                document.getElementById('output').style.color = 'blue'
+                document.getElementById('output').innerHTML = data.Message
             }
             // store the token created when user is logged in
-            if (data.status === "Success!") {
+            if (data.Status === 200) {
                 // if request is successful
                 window.location.assign("meetups.html");
             }
-            window.localStorage.setItem('token', data.token);
+            window.localStorage.setItem('token', data.access_token);
         })
 }
