021cc218dd241351f7172b85c59593c8ddee3b60
jonathanmusila <jonathanmusila6@gmail.com>
2019-02-13 13:17:44 +0300

    [Starts #163940461] Started the get all qsns

--- UI/static/js/meetups.js
+++ UI/static/js/meetups.js
@@ -25,7 +25,6 @@ fetch(meetupsUrl, {
         }
         if (data.status === 200) {
             // if request is successful
-            let meetups = data.meetups; // Get the meetups
             return data.data.forEach(function(meetup) { 
                 lst.innerHTML += `
                 <div class="column">
@@ -35,7 +34,7 @@ fetch(meetupsUrl, {
                 <p><i>Location: </i>${meetup.location}</p>
                 <p><i>Time Posted: </i>${meetup.time_added}</p>
                 <p>Intrested? <a href="#"><b>Click here to schedule</b></a></p>
-                <a href="questions.html"><button>View Questions</button></a>
+                <a onClick="getMeetup();"><button>View Questions</button></a>
             </div>
                 `;
             })
@@ -43,3 +42,47 @@ fetch(meetupsUrl, {
         }
 
     })
+
+
+//get a meetup
+function getMeetup() {
+    let meetupId = sessionStorage.getItem('meetupid');
+    let lt = document.getElementById('meetp');
+    let meUrl = `https://my-postgres-questioner-v2-api.herokuapp.com/api/v2/meetups/upcoming/${meetupId}`;
+    let token = window.localStorage.getItem('token');
+    fetch(meUrl, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.status === 404) {
+                // if request is unsuccessful
+                document.getElementById('output').style.color = 'blue'
+                document.getElementById('output').innerHTML = data.message
+                return message
+            }
+            if (data.status === 200) {
+                // if request is successful
+                return data.meetup.forEach(function(meetp) { 
+                    lt.innerHTML += `
+                    <div class="column">
+                    <p>${meetp.title}</p>
+                    <img src="${meetp.image}">
+                    <p><i>Date: </i>${meetp.happeningOn}</p>
+                    <p><i>Location: </i>${meetp.location}</p>
+                    <p><i>Time Posted: </i>${meetp.time_added}</p>
+                    <p>Intrested? <a href="#"><b>Click here to schedule</b></a></p>
+                </div>
+                    `;
+                })
+            }
+            setTimeout(function() {
+                window.location.assign("questions.html");
+            })
+        })
+}
+
