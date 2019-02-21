cfe5231dbcad5da579240d30c90d896ed9a4db8e
jonathanmusila <jonathanmusila6@gmail.com>
2019-02-21 13:11:45 +0300

    [Finishes #163940461] Added get all questions for a meetup

--- UI/static/js/meetups.js
+++ UI/static/js/meetups.js
@@ -1,88 +1,75 @@
  // display all available meetups
+    fetchMeetups()
 
-function stashId(inid) {
-    sessionStorage.setItem('meetupid', inid)
-}
-
-
-let lst = document.getElementById('meetups');
-let meetupsUrl = 'https://my-postgres-questioner-v2-api.herokuapp.com/api/v2/meetups/upcoming';
-let token = window.localStorage.getItem('token');
-fetch(meetupsUrl, {
-        method: 'GET',
-        headers: {
-            'Access-Control-Request-Headers': '*',
-            'Authorization': 'Bearer ' + token
+    function fetchMeetups(){
+      let url = 'https://my-postgres-questioner-v2-api.herokuapp.com/api/v2/meetups/upcoming';
+      let token = window.localStorage.getItem('token');
+    
+      fetch(url, {
+        method : 'GET',
+        headers : {
+          'Access-Control-Request-Headers': '*',
+          'Authorization': 'Bearer ' + token
         }
-    })
-    .then((res) => res.json())
-    .then((data) => {
+      })
+      .then((res) => res.json())
+      .then((data ) => {
+        if (data.status === 200){
+    
+          var meetups = document.getElementById('meetups')
+          var meetupNodes = document.createDocumentFragment()
+    
+          data.data.forEach(meetup => {
+    
+              var meetupNode = document.createElement('div')
+              meetupNode.className = 'column'
+
+              var topicNode = document.createElement('p')
+              topicNode.id = 'title'
+              topicNode.textContent = meetup.title
+
+              var timeNode = document.createElement('p')
+              timeNode.id = 'time_added'
+              timeNode.innerHTML = `<i>Date Posted:</i> ${meetup.time_added}`
+
+              var locationNode = document.createElement('p')
+              locationNode.id = 'location'
+              locationNode.innerHTML = `<i>Location:</i> ${meetup.location}`
+              
+              var anchorNode = document.createElement('a')
+              anchorNode.id = meetup.meetup_id.toString()
+              anchorNode.href = "../UI/questions.html?meetupid=" + meetup.meetup_id
+    
+              var imgNode = document.createElement('img')
+              imgNode.src = '../UI/static/images/meet.jpg'
+    
+              var dateNode = document.createElement('p')
+              dateNode.id = 'happeningOn'
+              dateNode.textContent = meetup.happeningOn
+              dateNode.innerHTML = `<i>Happening On:</i> ${meetup.happeningOn}`
+    
+    
+              anchorNode.appendChild(topicNode)
+              anchorNode.appendChild(imgNode)
+              anchorNode.appendChild(dateNode)
+              anchorNode.appendChild(timeNode)
+              anchorNode.appendChild(locationNode)
+    
+              meetupNode.appendChild(anchorNode)
+    
+              meetupNodes.appendChild(meetupNode)
+    
+          })
+    
+          meetups.appendChild(meetupNodes);
+    
+        } 
         if (data.status === 404) {
-            // if request is unsuccessful
-            document.getElementById('output').style.color = 'blue'
-            document.getElementById('output').innerHTML = data.message
-            return message
-        }
-        if (data.status === 200) {
-            // if request is successful
-            return data.data.forEach(function(meetup) { 
-                lst.innerHTML += `
-                <div class="column">
-                <p>${meetup.title}</p>
-                <img src="${meetup.image}">
-                <p><i>Date: </i>${meetup.happeningOn}</p>
-                <p><i>Location: </i>${meetup.location}</p>
-                <p><i>Time Posted: </i>${meetup.time_added}</p>
-                <p>Intrested? <a href="#"><b>Click here to schedule</b></a></p>
-                <a onClick="getMeetup();"><button>View Questions</button></a>
-            </div>
-                `;
-            })
-
-        }
-
-    })
-
-
-//get a meetup
-function getMeetup() {
-    let meetupId = sessionStorage.getItem('meetupid');
-    let lt = document.getElementById('meetp');
-    let meUrl = `https://my-postgres-questioner-v2-api.herokuapp.com/api/v2/meetups/upcoming/${meetupId}`;
-    let token = window.localStorage.getItem('token');
-    fetch(meUrl, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            }
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.status === 404) {
-                // if request is unsuccessful
-                document.getElementById('output').style.color = 'blue'
-                document.getElementById('output').innerHTML = data.message
-                return message
-            }
-            if (data.status === 200) {
-                // if request is successful
-                return data.meetup.forEach(function(meetp) { 
-                    lt.innerHTML += `
-                    <div class="column">
-                    <p>${meetp.title}</p>
-                    <img src="${meetp.image}">
-                    <p><i>Date: </i>${meetp.happeningOn}</p>
-                    <p><i>Location: </i>${meetp.location}</p>
-                    <p><i>Time Posted: </i>${meetp.time_added}</p>
-                    <p>Intrested? <a href="#"><b>Click here to schedule</b></a></p>
-                </div>
-                    `;
-                })
-            }
-            setTimeout(function() {
-                window.location.assign("questions.html");
-            })
-        })
-}
-
+          // if request is unsuccessful
+          document.getElementById('output').style.color = 'blue'
+          document.getElementById('output').innerHTML = data.message
+          return message
+      }
+      })
+    
+    } 
